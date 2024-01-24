96a34e34e2aca6018c140ccd1426fa47ddc42266
Shrinjay Shresth <shrinjayshresth1@gmail.com>
2024-01-25 00:46:27 +0530

    My first commit

---
+++ ARRAYS/MaximunIndexDifference.cpp
@@ -0,0 +1,10 @@
+//Given an array A[] of N positive integers.
+// The task is to find the maximum of j - i subjected to the constraint of A[i] < A[j] and i < j.
+#include<iostream>
+using namespace std;
+
+int maxIndexDiff(int A[], int N) 
+    { 
+        
+        
+    }
