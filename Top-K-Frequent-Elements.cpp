91326d1ccde2cee41ee147c6c88b08252699d1f1
Shrinjay Shresth <104555486+shrinjayshresth1@users.noreply.github.com>
2025-06-25 22:39:46 +0530

    [347] [Time Beats: 100.00%] [Memory Beats: 73.33%] - LeetPush

--- Top-K-Frequent-Elements.cpp
+++ Top-K-Frequent-Elements.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
-        vector<vector<int>>frequencyBucket(nums.size()+1);
-
-        //Populate the hashmaps
-        for(auto num :nums){
-            mp[num]++;
+        unordered_map<int, int> count;
+        for (int num : nums) {
+            count[num]++;
         }
 
-        //Populate the buckets according to their frequency
-        for(auto& itr:mp){
-            frequencyBucket[itr.second].push_back(itr.first);
-        }
-        vector<int>result;
-        //Traverse in reverse and collect K elements from the inner arrays
-        for(int i=frequencyBucket.size()-1;i>=0;i--){ // Changed i>0 to i>=0 to include i=0
-            for(int itr:frequencyBucket[i]){
-                result.push_back(itr);
-                if(result.size()==k){
-                    return result;
-                }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+        for (auto& entry : count) {
+            heap.push({entry.second, entry.first});
+            if (heap.size() > k) {
+                heap.pop();
             }
         }
-        return result; 
+
+        vector<int> res;
+        for (int i = 0; i < k; i++) {
+            res.push_back(heap.top().second);
+            heap.pop();
+        }
+        return res;
     }
 };
